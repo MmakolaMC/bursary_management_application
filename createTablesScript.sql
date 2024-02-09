422eb8c10c28be83c9227fa4ccf21fcd22696606
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2024-02-09 05:35:42 +0200

    BursaryYearlyBudget  Table

--- createTablesScript.sql
+++ createTablesScript.sql
@@ -3,8 +3,10 @@ GO
 
 CREATE TABLE [dbo].[Institutions](
 [instituteID] [int] IDENTITY(1,1) PRIMARY KEY  NOT NULL,
-[InstituteName] [varchar] (120),
-[AccountNumber] [Money] (120)
+[InstituteName] [varchar] (120) DEFAULT "not Available",
+[AccountNumber] [int] DEFAULT "Not Available",
+[AmountAllocated] [Money] DEFAULT 0,
+[AccountNumberToStudent] [Money] DEFAULT 0,
 )
 
 GO
@@ -12,10 +14,10 @@ GO
 
 CREATE TABLE [dbo].[HeadOfDepartment](
 [HeadOfDepartmentID] [int] IDENTITY(1,1) PRIMARY KEY  NOT NULL,
-[HeadOfDepartmentName] [varchar] (120),
-[HeadOfDepartmentEmail] [varchar] (120),
-[HeadOfDepartmentNumber] [int],
-InstituteID [int],
+[HeadOfDepartmentName] [varchar] (120) DEFAULT "Not Available",
+[HeadOfDepartmentEmail] [varchar] (120) DEFAULT "Not Available",
+[HeadOfDepartmentNumber] [int] DEFAULT "Not Available",
+InstituteID [int] DEFAULT "Not Available",
 FOREIGN KEY (InstituteID) REFERENCES Institutions(InstituteID)
 )
 
@@ -23,9 +25,28 @@ GO
 
 CREATE TABLE [dbo].[Applications](
 [ApplicantID] [int] IDENTITY(1,1) PRIMARY KEY  NOT NULL,
-[ApplicantName] [varchar] (120),
-[Status] [varchar] (120),
-[AmountAppliedFor] [Money],
+[ApplicantName] [varchar] (120) DEFAULT "Not Available",
+[Status] [varchar] (120)DEFAULT "Not Available",
+[AmountAppliedFor] [Money] DEFAULT 0
 )
 
 GO
+
+CREATE TABLE [dbo].[STUDENTS](
+[StudentID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+[StudentName] [varchar] (120) DEFAULT "Not Available",,
+[StudentNumber][int] DEFAULT "Not Available",,
+[StudentAge][int] DEFAULT "Not Available",,
+[Race][varchar] DEFAULT "Not Available",,
+[Documents] [varBinary] DEFAULT "Not Available",,
+[YearOfStudy] [date] DEFAULT "Not Available",
+)
+
+GO
+
+CREATE TABLE [dbo].[BursaryYearlyBudget](
+[BudgetID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+[DateAllocated] [date] DEFAULT "Not Available",
+[AmountAllocated][Money] DEFAULT 0,
+[AmountAllocatedToInstitute] [Money] DEFAULT 0,
+)
