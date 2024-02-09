b83d5d5425a968d99b849759810a24d7ef0ee07e
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2024-02-09 04:49:03 +0200

    created HeadOfDepartment Table

--- createTablesScript.sql
+++ createTablesScript.sql
@@ -3,8 +3,20 @@ GO
 
 CREATE TABLE [dbo].[Institutions](
 [instituteID] [int] IDENTITY(1,1) PRIMARY KEY  NOT NULL,
-[InstituteName] [varchar],
-[AccountNumber] [Money]
+[InstituteName] [varchar] (120),
+[AccountNumber] [Money] (120)
+)
+
+GO
+
+
+CREATE TABLE [dbo].[HeadOfDepartment](
+[HeadOfDepartmentID] [int] IDENTITY(1,1) PRIMARY KEY  NOT NULL,
+[HeadOfDepartmentName] [varchar] (120),
+[HeadOfDepartmentEmail] [varchar] (120),
+[HeadOfDepartmentNumber] [int],
+InstituteID [int],
+FOREIGN KEY (InstituteID) REFERENCES Institutions(InstituteID)
 )
 
 GO
