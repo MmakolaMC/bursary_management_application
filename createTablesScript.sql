eb876bd2cd990dc39bbe075ebcca134f24b67613
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2024-02-09 05:52:31 +0200

    remove Default values

--- createTablesScript.sql
+++ createTablesScript.sql
@@ -3,21 +3,20 @@ GO
 
 CREATE TABLE [dbo].[Institutions](
 [instituteID] [int] IDENTITY(1,1) PRIMARY KEY  NOT NULL,
-[InstituteName] [varchar] (120) DEFAULT "not Available",
-[AccountNumber] [int] DEFAULT "Not Available",
+[InstituteName] [varchar] (120) NULL,
+[AccountNumber] [int] NULL,
 [AmountAllocated] [Money] DEFAULT 0,
 [AccountNumberToStudent] [Money] DEFAULT 0,
 )
 
 GO
 
-
 CREATE TABLE [dbo].[HeadOfDepartment](
 [HeadOfDepartmentID] [int] IDENTITY(1,1) PRIMARY KEY  NOT NULL,
-[HeadOfDepartmentName] [varchar] (120) DEFAULT "Not Available",
-[HeadOfDepartmentEmail] [varchar] (120) DEFAULT "Not Available",
-[HeadOfDepartmentNumber] [int] DEFAULT "Not Available",
-InstituteID [int] DEFAULT "Not Available",
+[HeadOfDepartmentName] [varchar] (120) NULL,
+[HeadOfDepartmentEmail] [varchar] (120) NULL,
+[HeadOfDepartmentNumber] [int] NULL,
+InstituteID [int] NULL,
 FOREIGN KEY (InstituteID) REFERENCES Institutions(InstituteID)
 )
 
@@ -25,8 +24,8 @@ GO
 
 CREATE TABLE [dbo].[Applications](
 [ApplicantID] [int] IDENTITY(1,1) PRIMARY KEY  NOT NULL,
-[ApplicantName] [varchar] (120) DEFAULT "Not Available",
-[Status] [varchar] (120)DEFAULT "Not Available",
+[ApplicantName] [varchar] (120) NULL,
+[Status] [varchar] (120)NULL,
 [AmountAppliedFor] [Money] DEFAULT 0
 )
 
@@ -34,19 +33,23 @@ GO
 
 CREATE TABLE [dbo].[STUDENTS](
 [StudentID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
-[StudentName] [varchar] (120) DEFAULT "Not Available",,
-[StudentNumber][int] DEFAULT "Not Available",,
-[StudentAge][int] DEFAULT "Not Available",,
-[Race][varchar] DEFAULT "Not Available",,
-[Documents] [varBinary] DEFAULT "Not Available",,
-[YearOfStudy] [date] DEFAULT "Not Available",
+[StudentName] [varchar] (120) NULL,,
+[StudentNumber][int] NULL,,
+[StudentAge][int] NULL,,
+[Race][varchar] NULL,,
+[Documents] [varBinary] NULL,,
+[YearOfStudy] [date] DEFAULT GETDATE(),
 )
 
 GO
 
 CREATE TABLE [dbo].[BursaryYearlyBudget](
 [BudgetID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
-[DateAllocated] [date] DEFAULT "Not Available",
+[DateAllocated] [date] NULL,
 [AmountAllocated][Money] DEFAULT 0,
 [AmountAllocatedToInstitute] [Money] DEFAULT 0,
+InstituteID [int] NULL,
+FOREIGN KEY (InstituteID) REFERENCES Institutions(InstituteID)
 )
+
+GO
