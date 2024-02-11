cc38fe4eaab507ea9d6aebb7a18eea2e66f2fe8f
Magalaweng Clive Mmakola <clive.mmakola@bbd.co.za>
2024-02-11 18:02:40 +0200

    Fix create database script back

--- createDatabase.sql
+++ createDatabase.sql
@@ -1,5 +1,5 @@
 
-IF NOT EXISTS(SELECT 1 FROM sys.databases WHERE name = 'Bursary_Management_Application_DB')
+IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'Bursary_Management_Application_DB')
 BEGIN
     CREATE DATABASE Bursary_Managemeny_Application_DB;
 END;
