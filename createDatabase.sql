2407a2832605f6cb68f54815ee520c2f70a5a66f
Magalaweng Clive Mmakola <clive.mmakola@bbd.co.za>
2024-02-11 16:59:59 +0200

    Fix typo in sql script: *managemeny* to management

--- createDatabase.sql
+++ createDatabase.sql
@@ -1,5 +1,5 @@
 
-IF EXISTS(select * from sys.databases where name='Bursary_Managemeny_Application_DB')
+IF EXISTS(select * from sys.databases where name='Bursary_Management_Application_DB')
 DROP DATABASE BURSARY_MANAGEMENT_APPLICATION_DB
 
 CREATE DATABASE Bursary_Managemeny_Application_DB;
