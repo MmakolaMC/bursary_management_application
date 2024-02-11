1683bbbfda6fa94d777125e5eed420f0e0c9ade3
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2024-02-11 20:20:48 +0200

    added a insert data procedure

--- StoredProcedure.sql
+++ StoredProcedure.sql
@@ -16,3 +16,22 @@ END
 EXEC UpdateBudgetAmount;
 
 SELECT * FROM dbo.Budget;
+
+CREATE PROCEDURE UpdateBudgetAmount
+AS
+BEGIN 
+
+    UPDATE b
+    SET b.BudgetAmount = b.BudgetAmount - ISNULL(f.TotalAmountFunded, 0)
+    FROM dbo.Budget AS b
+    LEFT JOIN (
+        SELECT BudgetID, SUM(AmountFunded) AS TotalAmountFunded
+        FROM dbo.Funds
+        GROUP BY BudgetID
+    ) AS f ON b.BudgetID = f.BudgetID;
+END
+
+
+EXEC UpdateBudgetAmount;
+
+SELECT * FROM dbo.Budget;
