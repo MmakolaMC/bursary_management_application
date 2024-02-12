5123dc143a05c247330e055f501efa3feddb3bf6
Magalaweng Clive Mmakola <clive.mmakola@bbd.co.za>
2024-02-12 12:17:08 +0200

    Create procedure to insert data into desired table

--- InsertData.sql
+++ InsertData.sql
@@ -1,65 +1,23 @@
-
-
-INSERT INTO Institution (InstituteName)
-VALUES ('University of Limpopo'),
-       ('College of ABC'),
-       ('University of Johannesburg');
-
-
-INSERT INTO Race (RaceName)
-VALUES ('BLACK'),
-       ('COLOURED'),
-       ('INDIAN');
-  
-INSERT INTO Students (StudentName, StudentAge, InstitionID, RaceID)
-VALUES ('John Doe', 25, 1, 1),
-       ('Jane Smith', 22, 2, 2),
-       ('Michael Johnson', 20, 3, 3);
-
-INSERT INTO Documents (DocumentType, StudentID)
-VALUES (CONVERT(varbinary, 'Transcript'), 1),
-       (CONVERT(varbinary, 'Application Form'), 2),
-       (CONVERT(varbinary, 'ID Card'), 3);
-
-INSERT INTO Department (DepartmentName, InstitutionID)
-VALUES ('Computer Science', 1),
-       ('Engineering', 2),
-       ('Informatics ', 3);
-
-
-INSERT INTO HeadOfDepartment (HeadOfDepartmentName, DepartmentID)
-VALUES ('Dr. Smith', 1),
-       ('Prof. Coulter', 2),
-       ('Ms. Brown', 3);
-
-
-INSERT INTO Reviewer (ReviewerName, ReviewerNumber, ReviewerEmail)
-VALUES ('Mr. White', 123456789, 'white@example.com'),
-       ('Ms. Black', 987654321, 'black@example.com');
-
-
-INSERT INTO Budget (DateTime, BudgetAmount, ReviewerID)
-VALUES ('2023-01-01', 10000.00, 1);
-
-
-INSERT INTO status (statusName)
-VALUES ('Pending'),
-       ('Approved'),
-       ('Rejected');
-
-
-INSERT INTO Application ( status, DepartmentID, StudentID, StatusID)
-VALUES ( 'Pending', 1, 1, 1),
-       ('Approved', 2, 2, 2);
-
-INSERT INTO Funds (AmountFunded, DepartmentID, BudgetID)
-VALUES (3000.00, 1, 1),
-       (4000.00, 2, 1);
-
-
-INSERT INTO Programs (ProgramName, InstitutionID)
-VALUES ('Computer Science Program', 1),
-       ('Engineering Program', 2),
-       ('Informatics', 3);
-
-
+ALTER PROCEDURE [dbo].[InsertDataStoredProcedure](
+    @TableName varchar(max) = '',
+    @ColumnNames varchar(max) = '',
+    @Values varchar(max) = ''
+)
+AS
+BEGIN
+    IF @TableName = '' OR @ColumnNames = '' OR @Values = ''
+    BEGIN
+        PRINT 'Please provide valid parameters.'
+        RETURN
+    END
+ 
+    DECLARE @SQLQuery varchar(max)
+    SET @SQLQuery =  'INSERT INTO ' + @TableName + ' (' + @ColumnNames + ') VALUES (' + @Values + ')'
+    EXEC (@SQLQuery)
+END
+ 
+ 
+EXEC dbo.InsertDataIntoTable
+@TableName ='Reviewer',
+@ColumnNames= 'ReviewerName,ReviewerNumber,ReviewerEmail',
+@Values = '''Dr Smith'',0788927345, ''smith@examample.co.za''
